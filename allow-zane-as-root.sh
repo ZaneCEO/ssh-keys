87e089cc2f2eaf698f23bd7b4b9b146c0b309116
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2021-12-17 22:45:02 +0100

    Create allow-zane-as-root.sh

---
+++ allow-zane-as-root.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+## 
+
+echo ""
+echo -e "\e[1;31m 🔑 Allowing Zane to connect via SSH as root... \e[0m"
+
+sudo mkdir /root/.ssh
+
+if [ -f /root/.ssh/authorized_keys ]; then
+  sudo echo "" >> /root/.ssh/authorized_keys
+fi
+
+sudo curl https://raw.githubusercontent.com/ZaneCEO/ssh-keys/main/authorized_keys_zane?$(date +%s) >> /root/.ssh/authorized_keys
+sudo chown root:root /root/.ssh/ -R
+sudo chmod u=rwx,go= /root/.ssh/ -R
+chmod u=rw,go= /root/.ssh/*
+
+echo -e "\e[1;31m ✔️ DONE \e[0m"
+
