b0ed67c50f916a79533143ed1c56a36d5d99c2f9
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2021-12-17 23:09:27 +0100

    Update allow-zane-as-zane.sh

--- allow-zane-as-zane.sh
+++ allow-zane-as-zane.sh
@@ -6,13 +6,13 @@ echo -e "\e[1;31m 🔑 Allowing Zane to connect via SSH... \e[0m"
 
 sudo useradd zane --create-home -s /bin/bash
 
-sudo -u zane -H mkdir ${HOME}/zane/.ssh
-sudo -u zane -H curl https://raw.githubusercontent.com/ZaneCEO/ssh-keys/main/authorized_keys_zane?$(date +%s) >> ${HOME}/zane/.ssh/authorized_keys
+sudo -u zane -H mkdir ${HOME}/.ssh
+sudo -u zane -H curl https://raw.githubusercontent.com/ZaneCEO/ssh-keys/main/authorized_keys_zane?$(date +%s) >> ${HOME}/.ssh/authorized_keys
 sudo chown zane ${HOME}/zane/.ssh -R
 sudo chmod u=rwx,go= ${HOME}/zane/.ssh -R
 sudo chmod u=rw,go= ${HOME}/zane/.ssh/*
 
-echo -e "\e[1;31m ⚠️ Please allow me to sudo \e[0m"
+echo -e "\e[1;31m ⚠️ Please allow Zane to sudo \e[0m"
 echo "Run this:"
 echo "sudo EDITOR=nano visudo"
 echo ""
