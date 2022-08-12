fb68f7454d0359b0e05642f790a98b3d0f3ce7d2
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-08-12 22:13:27 +0200

    Create sudoer.sh

---
+++ sudoer.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# sudo apt install curl -y && curl -s https://raw.githubusercontent.com/ZaneCEO/ssh-keys/main/sudoer.sh?$(date +%s) | sudo bash
+ZANE_SUDOER_FILE=/etc/sudoers.d/zane
+
+sudo apt update
+sudo apt install curl -y
+sudo curl -Lo ${ZANE_SUDOER_FILE} https://raw.githubusercontent.com/ZaneCEO/ssh-keys/main/sudoer
+sudo chown root:root ${ZANE_SUDOER_FILE}
+chmod u=rw,go=r ${ZANE_SUDOER_FILE}
