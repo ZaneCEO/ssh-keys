cba72517f7cc126babb699be0570b4aa953f5dce
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2025-05-03 18:55:03 +0200

    Update sudoer.sh

--- sudoer.sh
+++ sudoer.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
-# sudo apt install curl -y && curl -s https://raw.githubusercontent.com/ZaneCEO/ssh-keys/main/sudoer.sh?$(date +%s) | sudo bash
 
-echo ""
-echo -e "\e[1;31m ⚠️ Allowing Zane to sudo \e[0m"
-
+## bash-fx
 if [ -z $(command -v curl) ]; then sudo apt update && sudo apt install curl -y; fi
+
+if [ -f "/usr/local/turbolab.it/bash-fx/bash-fx.sh" ]; then
+  source "/usr/local/turbolab.it/bash-fx/bash-fx.sh"
+else
+  source <(curl -s https://raw.githubusercontent.com/TurboLabIt/bash-fx/main/bash-fx.sh)
+fi
+## bash-fx is ready
+
+fxTitle "Allowing Zane to sudo"
 
 ZANE_SUDOER_FILE=/etc/sudoers.d/zane
 sudo curl -Lo ${ZANE_SUDOER_FILE} https://raw.githubusercontent.com/ZaneCEO/ssh-keys/main/sudoer
 sudo chown root:root ${ZANE_SUDOER_FILE}
-chmod u=rw,go=r ${ZANE_SUDOER_FILE}
+sudo chmod u=rw,go=r ${ZANE_SUDOER_FILE}
 
 echo ""
 ls -l $(dirname ${ZANE_SUDOER_FILE})
@@ -17,5 +23,4 @@ ls -l $(dirname ${ZANE_SUDOER_FILE})
 echo ""
 cat ${ZANE_SUDOER_FILE}
 
-echo ""
-echo -e "\e[1;31m ✔️ DONE \e[0m"
+fxOK "Done!"
